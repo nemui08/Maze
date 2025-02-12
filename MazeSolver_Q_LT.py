214721222c9695610c26326c9f046f76d3bed0d0
Natee <natee33067@gmail.com>
2025-02-12 23:37:46 +0700

    fix stack method

--- MazeSolver_Q_LT.py
+++ MazeSolver_Q_LT.py
@@ -8,7 +8,7 @@ def bfs(maze_obj):
     visited.add((maze_obj.ply.y, maze_obj.ply.x))
     parent = {}
 
-    while not queue.is_empty():
+    while not queue.isEmpty():
         current_pos = queue.dequeue()
         if (current_pos.y, current_pos.x) == (maze_obj.end.y, maze_obj.end.x):
             while current_pos != maze_obj.ply:
@@ -17,14 +17,14 @@ def bfs(maze_obj):
             maze_obj.maze[maze_obj.ply.y][maze_obj.ply.x] = "P"
             maze_obj.draw_maze()
             maze_obj.draw_player()
-            maze_obj.print_end()
+            maze_obj.printEND()
             return True
 
         for move in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             new_y = current_pos.y + move[0]
             new_x = current_pos.x + move[1]
 
-            if maze_obj.is_in_bound(new_y, new_x) and (new_y, new_x) not in visited:
+            if maze_obj.isInBound(new_y, new_x) and (new_y, new_x) not in visited:
                 if maze_obj.maze[new_y][new_x] == " " or maze_obj.maze[new_y][new_x] == "E":
                     queue.enqueue(pos(new_y, new_x))
                     visited.add((new_y, new_x))
