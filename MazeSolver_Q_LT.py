2e27598f79408af6a68b122677070e697a31f4a1
Natee <natee33067@gmail.com>
2025-02-12 23:41:11 +0700

    fix method

--- MazeSolver_Q_LT.py
+++ MazeSolver_Q_LT.py
@@ -17,14 +17,14 @@ def bfs(maze_obj):
             maze_obj.maze[maze_obj.ply.y][maze_obj.ply.x] = "P"
             maze_obj.draw_maze()
             maze_obj.draw_player()
-            maze_obj.printEND()
+            maze_obj.print_end()
             return True
 
         for move in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             new_y = current_pos.y + move[0]
             new_x = current_pos.x + move[1]
 
-            if maze_obj.isInBound(new_y, new_x) and (new_y, new_x) not in visited:
+            if maze_obj.is_in_bound(new_y, new_x) and (new_y, new_x) not in visited:
                 if maze_obj.maze[new_y][new_x] == " " or maze_obj.maze[new_y][new_x] == "E":
                     queue.enqueue(pos(new_y, new_x))
                     visited.add((new_y, new_x))
